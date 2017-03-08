8935cb59ac011fed9567495163bf9a1ca696be26
Alex Contursi <contursi.axel@gmail.com>
2017-03-08 16:05:15 +0100

    collegamento css e javascript

--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ var app = express();
 var path = require('path');
 var app = express();
 
-app.listen(3000,function(err){
-  console.log('server connesso alla porta 3000');
-});
+
 //bootstrap
 app.use('/bootstrap',express.static(path.join(__dirname,"..","node_modules","bootstrap","dist")));
 //JQUERY
 app.use('/jquery',express.static(path.join(__dirname,"..","node_modules","jquery","dist")));
 
+//js css
+app.use('/js',express.static(path.join(__dirname,"..","client","js")));
+app.use('/css',express.static(path.join(__dirname,"..","client","css")));
 //serviamo la INDEX
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,"..","client","index.html"));
+});
+
+app.listen(3000,function(err){
+  console.log('server connesso alla porta 3000');
 });
