c4517d2d7adbef4109b1cc050104f7c56fb1c35e
Alex Contursi <contursi.axel@gmail.com>
2017-03-08 18:02:44 +0100

    quasi finto

--- server/Users/index.js
+++ server/Users/index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 var Users =  require('./Users.controller.js');
 
-//lista Utenti
-router.get('/',function(req,res){
-  res.send('utenti');
-});
-
+//lista utenti
 router.get('/',Users.getUsers);
-
+//crea utente
+router.post('/',Users.createUsers);
+//dettaglio utente
+router.get('/:id',Users.detailUsers);
+//elimina utente
+router.delete('/:id',Users.deleteUsers);
+//modifica/aggiorna utente
+router.put('/:id',Users.updateUsers);
 
 
 
--- server/Users/users.controller.js
+++ server/Users/users.controller.js
@@ -1,9 +1,35 @@
 module.exports = (function(){
+  //stampa lista utente
   var getUsers = function(req,res){
     res.send('Lista Utenti');
   };
+  //crea utente
+  var createUsers = function(req,res){
+    res.send('creazione utente');
+  };
+  //dettaglio utente
+  var detailUsers = function(req,res){
+    var id = req.params.id;
+    res.send('dettaglio utente');
+  }
+  //elimina utente
+  var deleteUsers = function(req,res){
+    var id = req.params.id;
+    res.send('utente eliminato');
+  }
+  //modifica/aggiorna utente
+  var updateUsers = function(req,res){
+    var id = req.params.id;
+    res.send('aggiornamento riuscito');
+  }
+
+
   return{
-    getUsers : getUsers
+    getUsers : getUsers, //variabili pubbliche che si collega alla funzione che sta sopra
+    createUsers: createUsers,
+    detailUsers: detailUsers,
+    deleteUsers: deleteUsers,
+    updateUsers: updateUsers
 
   }
 
