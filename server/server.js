c14ab20308da4551058ada4e31a8fd47257c8216
Alex Contursi <contursi.axel@gmail.com>
2017-03-08 15:54:24 +0100

    inserito bootstrap e jquery

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ var app = express();
 app.listen(3000,function(err){
   console.log('server connesso alla porta 3000');
 });
-
+//bootstrap
+app.use('/bootstrap',express.static(path.join(__dirname,"..","node_modules","bootstrap","dist")));
+//JQUERY
+app.use('/jquery',express.static(path.join(__dirname,"..","node_modules","jquery","dist")));
 
 //serviamo la INDEX
 app.get('/',function(req,res){
